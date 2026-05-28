1dcf16aa5decb967f6ef81d66481a68aa69a2219
PlanetMetrics <wlien@bren.ucsb.edu>
2026-05-27 22:35:06 -0700

    commit

---
+++ ref/世豐水庫.py
@@ -0,0 +1,106 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      user
+#
+# Created:     21/10/2022
+# Copyright:   (c) user 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+# W1 is the lower weir
+W1_height == 14 #m
+W1_volume == 967400 #m3
+W1_watershed == 242 #km2
+W1_flow == 24.3 #CMS
+W1_capacity == 18.1 #MW
+Low_eif == 0.48 #CMS
+
+# W2 is the upper weir
+W2_height == 20 #m
+W2_volume == 237300 #m3
+W2_watershed == 31 #km2
+W2_flow == 6.3 #CMS
+W2_capacity == 19 #MW
+Up_eif == 0.06 #CMS
+
+#Setting Decade Perameters
+D_time = 60*60*12*12
+Low_D01a =
+Low_D01b =
+Low_D01c =
+Low_D02a =
+Low_D02b =
+Low_D02c =
+Low_D03a =
+Low_D03b =
+Low_D03c =
+Low_D04a =
+Low_D04b =
+Low_D04c =
+Low_D05a =
+Low_D05b =
+Low_D05c =
+Low_D06a =
+Low_D06b =
+Low_D06c =
+Low_D07a =
+Low_D07b =
+Low_D07c =
+Low_D08a =
+Low_D08b =
+Low_D08c =
+Low_D09a =
+Low_D09b =
+Low_D09c =
+Low_D10a =
+Low_D10b =
+Low_D10c =
+Low_D11a =
+Low_D11b =
+Low_D11c =
+Low_D12a =
+Low_D12b =
+Low_D12c =
+
+#Season Settings
+
+## Wet Season MAY~NOV
+Up_wS_min == 0.41
+Up_wS_max == 4.15
+Low_wS_min == 3.16
+Low_wS_max == 31.22
+
+
+
+## Dry Season
+Up_dS_min == 0.06
+Up_dS_max == 0.18
+Low_dS_min == 0.48
+Low_dS_max == 1.92
+
+#MAIN CALCULATION
+
+W1_volume == 967400 #m3
+W1_flow == 24.3 #CMS
+W1_Vneed = D_time * W1_flow
+
+print W1_Vneed
+
+
+
+
+
+
+
+
+
+
+
